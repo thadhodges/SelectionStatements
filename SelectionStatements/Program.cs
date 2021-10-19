9e7fe135002207073ceb0028c0b9d0a0553469f9
Thad <thadisfree@gmail.com>
2021-10-19 10:10:14 -0500

    Keyboard error

--- SelectionStatements/Program.cs
+++ SelectionStatements/Program.cs
@@ -6,7 +6,7 @@ namespace SelectionStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WridteLine("Hello World!");fg
         }
     }
 }
